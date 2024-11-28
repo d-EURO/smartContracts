ab1da4c1b4a04b16f71eccdb5e3accb459d064f4
lapatric <42653152+lapatric@users.noreply.github.com>
2024-11-28 23:35:58 +0100

    fix verify script when no args

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -1,12 +1,17 @@
 import { run } from "hardhat";
 
-export const verify = async (contractAddress: string, args: any) => {
+export const verify = async (contractAddress: string, args: any[] | undefined) => {
     console.log("Verifying contract...");
     try {
-        await run("verify:verify", {
+        const verificationParams: any = {
             address: contractAddress,
-            constructorArguments: args,
-        });
+        };
+
+        if (args && args.length > 0) {
+            verificationParams.constructorArguments = args;
+        }
+
+        await run("verify:verify", verificationParams);
     } catch (e: any) {
         if (e.message.toLowerCase().includes("already verified")) {
             console.log("Already Verified!");
