da20f7d5eb33965302c2e2b8e355621687948d54
Konstantin Ullrich <konstantinullrich12@gmail.com>
2025-01-01 21:35:17 +0100

    Test Fix

--- contracts/test/PositionExpirationTest.sol
+++ contracts/test/PositionExpirationTest.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 
 import {Position} from "../MintingHubV2/Position.sol";
 import {MintingHubGateway} from "../gateway/MintingHubGateway.sol";
+import {IMintingHubGateway} from "../gateway/interface/IMintingHubGateway.sol";
 import {IDecentralizedEURO} from "../interface/IDecentralizedEURO.sol";
 import {TestToken} from "./TestToken.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 contract PositionExpirationTest {
     MintingHubGateway public hub;
@@ -22,19 +24,35 @@ contract PositionExpirationTest {
         frontendCode = frontendCode_;
         col.mint(address(this), 100);
         col.approve(address(hub), 100);
-        address pos = hub.openPosition(
-            address(col),
-            10,
-            100 /* collateral */,
-            1000000 * 10 ** 18,
-            7 days,
-            30 days,
-            10 hours,
-            50000,
-            1000 * 10 ** 36 /* price */,
-            200000,
-            frontendCode
-        );
+        address pos;
+        if (IERC165(hub).supportsInterface(type(IMintingHubGateway).interfaceId)) {
+            pos = hub.openPosition(
+                address(col),
+                10,
+                100 /* collateral */,
+                1000000 * 10 ** 18,
+                7 days,
+                30 days,
+                10 hours,
+                50000,
+                1000 * 10 ** 36 /* price */,
+                200000,
+                frontendCode
+            );
+        } else {
+            pos = hub.openPosition(
+                address(col),
+                10,
+                100 /* collateral */,
+                1000000 * 10 ** 18,
+                7 days,
+                30 days,
+                10 hours,
+                50000,
+                1000 * 10 ** 36 /* price */,
+                200000
+            );
+        }
         Position(pos).transferOwnership(owner);
         return pos;
     }
