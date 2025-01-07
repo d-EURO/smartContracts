06c6435762cd44dd66b94ebda6117e9a4ea29c8a
lapatric <42653152+lapatric@users.noreply.github.com>
2025-01-07 16:44:04 +0100

    improvements

--- exports/index.ts
+++ exports/index.ts
@@ -5,8 +5,8 @@ export * from "./address.config";
 export * from "./abis/core/DecentralizedEURO";
 export * from "./abis/core/Equity";
 export * from "./abis/core/FrontendGateway";
-export * from "./abis/core/SavingsGateway";
 export * from "./abis/core/MintingHubGateway";
+export * from "./abis/core/SavingsGateway";
 
 export * from "./abis/MintingHubV2/PositionFactoryV2";
 export * from "./abis/MintingHubV2/PositionRoller";
@@ -19,6 +19,8 @@ export * from "./abis/utils/Leadrate";
 export * from "./abis/utils/UniswapV3Pool";
 export * from "./abis/utils/StablecoinBridge";
 export * from "./abis/utils/DEPSWrapper";
+export * from "./abis/utils/MintingHubV2";
+export * from "./abis/utils/Savings";
 
 // constructor args
 // export * from './ignition/constructor-args/index';
