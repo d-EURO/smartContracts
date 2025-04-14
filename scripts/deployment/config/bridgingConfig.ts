8fed9fab50783def35f3ff64a754cdad45766281
lapatric <42653152+lapatric@users.noreply.github.com>
2025-04-15 00:16:09 +0200

    Contract and deploy script for bridging DEPS

--- scripts/deployment/config/bridgingConfig.ts
+++ scripts/deployment/config/bridgingConfig.ts
@@ -33,3 +33,39 @@ export const bridgingConfig: BridgeConfig = {
     },
   },
 };
+
+interface BridgeDEPSConfig {
+  mainnetDEPS: string;
+  chains: {
+    [key: string]: {
+      chainId: number;
+      bridge: string;
+      token: {
+        name: string;
+        symbol: string;
+      };
+    };
+  };
+}
+
+export const bridgingDEPSConfig: BridgeDEPSConfig = {
+  mainnetDEPS: '0x103747924E74708139a9400e4Ab4BEA79FFFA380',
+  chains: {
+    optimism: {
+      chainId: 10,
+      bridge: '0x4200000000000000000000000000000000000010',
+      token: {
+        name: 'Decentralized Euro Protocol Share',
+        symbol: 'DEPS',
+      },
+    },
+    base: {
+      chainId: 8453,
+      bridge: '0x4200000000000000000000000000000000000010',
+      token: {
+        name: 'Decentralized Euro Protocol Share',
+        symbol: 'DEPS',
+      },
+    },
+  },
+};
