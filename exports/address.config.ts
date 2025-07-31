c1aa3fc59c342aa01ce370142facd7a2f51239e6
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2025-07-31 15:41:03 +0200

    Update address.config.ts

--- exports/address.config.ts
+++ exports/address.config.ts
@@ -16,6 +16,7 @@ export interface ChainAddress {
   bridgeEUROP: Address;
   bridgeEURI: Address;
   bridgeEURE: Address;
+  bridgeEURA: Address;
   eurt: Address;
   eurs: Address;
   veur: Address;
@@ -24,6 +25,7 @@ export interface ChainAddress {
   europ: Address;
   euri: Address;
   eure: Address;
+  eura: Address;
   roller: Address;
   positionFactoryV2: Address;
 }
@@ -38,7 +40,7 @@ export const ADDRESS: Record<number, ChainAddress> = {
     mintingHubGateway: "0x8B3c41c649B9c7085C171CbB82337889b3604618",
     DEPSwrapper: "0x103747924E74708139a9400e4Ab4BEA79FFFA380",
     bridgeEURT: "0x2353D16869F717BFCD22DaBc0ADbf4Dca62C609f",
-    bridgeEURS: "0x0423F419De1c44151B6b000e2dAA51859C1D5d2A",
+    bridge: "0x0423F419De1c44151B6b000e2dAA51859C1D5d2A",
     bridgeVEUR: "0x3Ed40fA0E5C803e807eBD51355E388006f9E1fEE",
     bridgeEURC: "0xD03cD3ea55e67bC61b78a0d70eE93018e2182Dbe",
     bridgeEURR: "0x20B0a153fF16c7B1e962FD3D3352A00cf019f1a7",
@@ -53,6 +55,7 @@ export const ADDRESS: Record<number, ChainAddress> = {
     europ: "0x888883b5F5D21fb10Dfeb70e8f9722B9FB0E5E51",
     euri: "0x9d1A7A3191102e9F900Faa10540837ba84dCBAE7",
     eure: "0x3231Cb76718CDeF2155FC47b5286d82e6eDA273f",
+    eura: "0x1a7e4e63778b4f12a199c062f3efdd288afcbce8",
     roller: "0x4CE0AB2FC21Bd27a47A64F594Fdf7654Ea57Dc79",
     positionFactoryV2: "0x167144d66AC1D02EAAFCa3649ef3305ea31Ee5A8",
   },
