14ed62825b296e96e693f1330e17cac5bfddcb96
lapatric <42653152+lapatric@users.noreply.github.com>
2025-08-18 12:44:19 +0200

    [DEV-4049] remove monitoring v2

--- scripts/monitoringV2/dto/depsWrapper.dto.ts
+++
@@ -1,9 +0,0 @@
-export interface DEPSWrapperState {
-  address: string;
-  name: string;
-  symbol: string;
-  decimals: number;
-  totalSupply: bigint;
-  underlyingAddress: string;
-  underlyingSymbol: string;
-}
